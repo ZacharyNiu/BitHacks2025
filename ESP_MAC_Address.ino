9e1aeae0544a1c544818276b5bc4bd9d66eaeec6
KatieKT <106551735+KaileyTR@users.noreply.github.com>
2025-04-12 22:31:09 -0700

    Add files via upload

---
+++ ESP_MAC_Address.ino
@@ -0,0 +1,16 @@
+#include <esp_now.h>
+#include <WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);  // ESP-NOW requires station mode
+  wifi.begin();
+  delay(100);
+  Serial.println("ESP32 Receiver MAC Address:");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {
+  // Nothing here
+}
+
